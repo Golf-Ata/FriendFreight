0777e18189e49c3aa244cee7cea6c33f89e6977e
Goffi <Goffi@Goffis-MacBook-Pro.local>
2017-02-04 14:08:57 +0700

    -   Add "FriendFreight.swift" and "ImportService.swift" to the project

---
+++ FriendFreight/FriendFreight.swift
@@ -0,0 +1,60 @@
+//
+//  FriendFreight.swift
+//  FriendFreight
+//
+//  Created by Goffi on 2/4/2560 BE.
+//  Copyright © 2560 Goffi. All rights reserved.
+//
+
+import Foundation
+
+struct UserNameType {
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ FriendFreight/ImportService.swift
@@ -0,0 +1,75 @@
+//
+//  ImportService.swift
+//  FriendFreight
+//
+//  Created by Goffi on 2/4/2560 BE.
+//  Copyright © 2560 Goffi. All rights reserved.
+//
+
+import Foundation
+
+
+// Protocols
+
+protocol importServiceUserType {
+    
+    var userName: UserNameType { get }
+    
+}
+
+
+// Error Type
+
+
+
+
+// Concrete Type
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
